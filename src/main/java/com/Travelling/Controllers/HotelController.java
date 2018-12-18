e6d55c05a05d803d866a35724fc0723f5ed51f73
XinmengLyu <lvxinmeng_1995@163.com>
2018-12-18 16:49:29 -0500

    implemented a hotel list.

--- src/main/java/com/Travelling/Controllers/HotelController.java
+++ src/main/java/com/Travelling/Controllers/HotelController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController("/hotel")
 public class HotelController {
     @Autowired
@@ -16,7 +19,14 @@ public class HotelController {
     @RequestMapping(value= "/hotel", method= {RequestMethod.GET, RequestMethod.POST})
     public ModelAndView hotel() {
         ModelAndView view = new ModelAndView("hotel");
-        view.addObject("place", new Place(0, "Holiday Inn", "848-111-1111", "www...", "String address", "String city", "String state", "String country", "String zip", 0d, 0d));
+        List<Place> hotelList = new ArrayList<>();
+        hotelList.add(new Place(0, "Holiday Inn1", "tel", "web", "address", "city", "state", "country", "zip", 0d, 0d));
+        hotelList.add(new Place(1, "Holiday Inn2", "tel", "web", "address", "city", "state", "country", "zip", 0d, 0d));
+        hotelList.add(new Place(2, "Holiday Inn3", "tel", "web", "address", "city", "state", "country", "zip", 0d, 0d));
+        hotelList.add(new Place(3, "Holiday Inn4", "tel", "web", "address", "city", "state", "country", "zip", 0d, 0d));
+        hotelList.add(new Place(4, "Holiday Inn5", "tel", "web", "address", "city", "state", "country", "zip", 0d, 0d));
+        hotelList.add(new Place(5, "Holiday Inn6", "tel", "web", "address", "city", "state", "country", "zip", 0d, 0d));
+        view.addObject("hotel_list", hotelList);
         return view;
     }
 }
